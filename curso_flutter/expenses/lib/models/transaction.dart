cc639f9ff7c946808ad6f4a1d0b79780e5b3a685
wanderleySilva-silva <jwps.17@hotmail.com>
2021-07-19 12:00:01 -0300

    Pequena modificação na classe Transaction

--- curso_flutter/expenses/lib/models/transaction.dart
+++ curso_flutter/expenses/lib/models/transaction.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/foundation.dart';
-
 class Transaction {
   final String id;
   final String title;
   final double value;
   final DateTime date;
 
-  Transaction(
-    @required this.id,
+
+  Transaction({
+    this.id,
     @required this.title,
-    @required this.value,
-    @required this.date,
-  );
+    this.value,
+    this.date
+  })
 }
