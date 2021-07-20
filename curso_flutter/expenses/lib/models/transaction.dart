553cdb7dd16f54e8fc2c99538b40bea935dcfa6b
wanderleySilva-silva <jwps.17@hotmail.com>
2021-07-20 11:25:22 -0300

    Foi necessário atualizar novamente a classe Transaction, pois alterei novamente a versão do flutter.

--- curso_flutter/expenses/lib/models/transaction.dart
+++ curso_flutter/expenses/lib/models/transaction.dart
@@ -1,14 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
 class Transaction {
   final String id;
   final String title;
   final double value;
   final DateTime date;
 
-
-  Transaction({
-    this.id,
-    @required this.title,
-    this.value,
-    this.date
-  })
+  Transaction(this.id, @required this.title, this.value, this.date);
 }
